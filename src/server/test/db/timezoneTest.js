ab2c5aa6def552dee9d257750d3f1bc7b2d75ebc
Wadood <alamw@beloit.edu>
2020-11-29 18:48:14 +0500

    timezone tests, but with error

---
+++ src/server/test/db/timezoneTest.js
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+ const { mocha, expect, testDB } = require('../common');
+ const Timezone = require('../../models/Timezone');
+ 
+ 
+ mocha.describe('Timezone', () => {
+    mocha.it('timezones contained in the array', async () => {
+         conn = testDB.getConnection();
+         
+         const expectedreading1 = new Timezone("GB", "GMT" ,timestamp1);
+         const expectedreading2 = new Timezone("posix/Africa/Bangui", "WAT" ,timestamp2); 
+         const expectedreading3 = new Timezone("posix/Africa/Harare", "CAT" ,timestamp3 );
+        
+         const timezoneReadings = await Timezone.getAllTimezones(conn);
+
+         expect( (timezoneReadings).includes(expectedreading1) );
+         expect( (timezoneReadings).includes(expectedreading2) );
+         expect( (timezoneReadings).includes(expectedreading3) );
+     });
+   
+ }); 
